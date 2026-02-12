24f049afb1da050d418e487af713a2ecee45c832
DevGPA <94722785+DevGPA@users.noreply.github.com>
2026-02-11 18:39:11 -0600

    Contexto Copy

--- Scripts/client_script_invoice.js
+++ Scripts/client_script_invoice.js
@@ -3,20 +3,36 @@
  * @NScriptType ClientScript
  */
 define(['N/currentRecord'], function(currentRecord) {
-    function fieldChanged(context) {
-        var rec = context.currentRecord;
-        // Verificar que estamos en la sublista de artículos y que cambió el campo 'item'
-        if (context.sublistId === 'item' && context.fieldId === 'item') {
-            var itemId = rec.getCurrentSublistValue({
+    function pageInit(scriptContext) {
+        // Solo ejecutar en modo 'edit' para evitar errores en 'view'
+        if (scriptContext.mode !== 'copy') return;
+
+        var rec = scriptContext.currentRecord;
+        var lineCount = rec.getLineCount({ sublistId: 'item' });
+
+        for (var i = 0; i < lineCount; i++) {
+            // 1. Seleccionar la línea específica
+            rec.selectLine({
                 sublistId: 'item',
-                fieldId: 'item'
+                line: i
             });
 
-            if (itemId) {
-                // Establecer el valor en otro campo de la misma línea
-                alert(itemId)
-            }
+            alert('Carga: '+i);
+
+            // 2. Establecer el nuevo valor en el campo deseado
+            rec.setCurrentSublistText({
+                sublistId: 'item',
+                fieldId: 'custcol_mx_txn_line_sat_tax_object',
+                text: '02 - Sí objeto de impuesto.',
+                ignoreFieldChange: true
+            });
+
+            // 3. Confirmar los cambios en la línea
+            rec.commitLine({ sublistId: 'item' });
         }
     }
-    return { fieldChanged: fieldChanged };
+
+    return {
+        pageInit: pageInit
+    };
 });
