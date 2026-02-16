abdeffde2d1300d002eddc23c09dec016faa88dd
DevGPA <94722785+DevGPA@users.noreply.github.com>
2026-02-15 23:10:43 -0600

    Corrección de Type

--- Scripts/user_event_script_po_contract.js
+++ Scripts/user_event_script_po_contract.js
@@ -2,30 +2,38 @@
  * @NApiVersion 2.x
  * @NScriptType UserEventScript
  */
-define(['N/record', 'N/log'], function(record, log) {
+define(['N/record', 'N/log'], function (record, log) {
     function beforeSubmit(context) {
         // Ejecutar solo al crear o editar
-        if (context.type === context.UserEventTypes.CREATE || context.type === context.UserEventTypes.EDIT) {
-            var newRecord = context.newRecord;
+        try {
+            if (context.type === context.UserEventType.CREATE || context.type === context.UserEventType.EDIT) {
+                var newRecord = context.newRecord;
 
-            // 1. Obtener el ID del Contrato
-            var contractId = newRecord.getValue({ fieldId: 'purchasecontract' });
+                // 1. Obtener el ID del Contrato
+                var contractId = newRecord.getValue({ fieldId: 'purchasecontract' });
 
-            // 2. Validar y cambiar estado de aprobación
-            if (contractIdract) {
-                newRecord.setValue({
-                    fieldId: 'approvalstatus',
-                    value: 2 // Estado: Aprobado
-                });
+                // 2. Validar y cambiar estado de aprobación
+                if (contractId) {
+                    newRecord.setValue({
+                        fieldId: 'approvalstatus',
+                        value: 2 // Estado: Aprobado
+                    });
 
-                log.debug({
+                    log.debug({
                         title: 'Éxito',
                         details: 'Estado de aprobación actualizado a Aprobado OC con Contrato'
-                });
+                    });
+                }
             }
         }
+    
+        catch (e) {
+            log.error({
+                title: 'Error en auto-aprobación',
+                details: e.message
+            });
+        }
     }
-
     return {
         beforeSubmit: beforeSubmit
     };
